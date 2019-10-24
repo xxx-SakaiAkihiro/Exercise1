bc8586b0cc390f530905c92b0041f81233b18fdc
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2019-10-24 15:19:39 +0900

    練習用

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+
+public class Car {
+
+	private int speed;
+	private String name;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+}
